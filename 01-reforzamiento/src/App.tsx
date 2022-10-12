64e3c00aedcf1c92281cd4ffd2b5016d35bc8be5
JeferProgramer <jefer.talento.11@gmail.com>
2022-10-12 10:07:17 -0500

    feat changes in app tsx

--- 01-reforzamiento/src/App.tsx
+++ 01-reforzamiento/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import TiposBosicos from './typescript/TiposBosicos'
 
 const App = () => {
   return (
     <div className='mt-2'>
       <h1>Introduccion en typescript</h1>
+      <TiposBosicos/>
     </div>
   )
 }
