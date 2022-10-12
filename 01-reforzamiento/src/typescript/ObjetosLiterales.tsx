6093cc044b18b94de5e43ff5f6e2fbb54268510b
JeferProgramer <jefer.talento.11@gmail.com>
2022-10-12 10:37:55 -0500

    feat add json for the visualization

--- 01-reforzamiento/src/typescript/ObjetosLiterales.tsx
+++ 01-reforzamiento/src/typescript/ObjetosLiterales.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 export const ObjetosLiterales = () => {
+
+  const person = {
+    nombre: "Fernando",
+    edad: 35,
+    direccion:{
+        pais: "Canada",
+        casaNo: 615
+    }
+  }
   return (
     <div>
         <h3>Objetos Literales</h3>
+        <code>
+            <pre>
+                {JSON.stringify(person, null, 2)}
+            </pre>
+        </code>
     </div>
   )
 }
