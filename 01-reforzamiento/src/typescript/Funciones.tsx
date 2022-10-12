44c417dc5096d24bf0fd2a0cdbc3ccf6ab888766
JeferProgramer <jefer.talento.11@gmail.com>
2022-10-12 11:22:16 -0500

    feat finish component funciones

--- 01-reforzamiento/src/typescript/Funciones.tsx
+++ 01-reforzamiento/src/typescript/Funciones.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Funciones = () => {
+
+  const sumar  = (a:number, b:number):number => {
+    return a+b
+  }
   return (
     <div>
       <h3>Funciones</h3>
+      <span>el Resultado es : { sumar(2,5) }</span>
     </div>
   )
 }
