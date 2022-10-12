8c3de53324f0e6b950478dbe21b2bed4c7b7cf3a
JeferProgramer <jefer.talento.11@gmail.com>
2022-10-12 10:14:51 -0500

    feat add types in typescript

--- 01-reforzamiento/src/typescript/TiposBosicos.tsx
+++ 01-reforzamiento/src/typescript/TiposBosicos.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const TiposBosicos = () => {
+  
+  let nombre: string | number = "Jeferson Cañon"
+
   return (
     <div>
       <h3>Tipos Basicos</h3>
+      {nombre}
     </div>
   )
 }
