ef99d719f434d14f0d6cab67cbb0b99795844b56
JeferProgramer <jefer.talento.11@gmail.com>
2022-10-12 11:21:37 -0500

    feat add component counter in app

--- 01-reforzamiento/src/App.tsx
+++ 01-reforzamiento/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import TiposBosicos from './typescript/TiposBosicos'
+// import TiposBosicos from './typescript/TiposBosicos'
+import Contador from './components/Contador';
 
 const App = () => {
   return (
     <div className='mt-2'>
       <h1>Introduccion en typescript</h1>
       <hr />
-      <TiposBosicos/>
+      {/* <TiposBosicos/> */}
+      <Contador/>
     </div>
   )
 }
