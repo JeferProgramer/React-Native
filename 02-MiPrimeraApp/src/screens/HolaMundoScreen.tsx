feb0c5ec3211e5e8a27a57f472dc1059970c9fd0
JeferProgramer <jefer.talento.11@gmail.com>
2022-10-18 11:17:54 -0500

    feat add comoponent hola mundo

---
+++ 02-MiPrimeraApp/src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HolaMundoScreen = () => {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+    }}>
+      <Text style={{
+        fontSize: 45,
+        textAlign: 'center',
+      }}>
+        Hola Jeferson
+      </Text>
+    </View>
+  );
+};
+
+export default HolaMundoScreen;
