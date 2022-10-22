ff1053db1b75b779ea33f07e4b841a18d7652ecc
JeferProgramer <jefer.talento.11@gmail.com>
2022-10-22 18:02:13 -0500

    feat add screen for the pagina 1 screen+

---
+++ 04-navegacionApp/src/screens/Pagina1Screen.tsx
@@ -0,0 +1,90 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable jsx-quotes */
+import { StackScreenProps } from '@react-navigation/stack';
+import React ,{useEffect} from 'react';
+import { Text, View , Button, TouchableOpacity} from 'react-native';
+import { styles, colores } from '../theme/appTheme';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+interface Props extends StackScreenProps <any, any>{}
+
+const Pagina1Screen = ({navigation}: Props) => {
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <TouchableOpacity
+          style={{
+            marginLeft: 10,
+          }}
+          onPress={() => navigation.toggleDrawer()}
+        >
+          <Icon
+            name="menu-outline"
+            color={colores.primary}
+            size={35}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
+
+  return (
+    <View style={styles.globalMargin}>
+
+      <Text style={styles.title}>Pagina 1 Screen</Text>
+
+      <Button
+        title='Ir pagina 2'
+        onPress={() => navigation.navigate('Pagina2Screen')}
+      />
+
+      <Text style={{
+        marginVertical: 20,
+        fontSize: 20,
+      }}> Navegar con argumentos
+      </Text>
+
+      <View style={{flexDirection: 'row'}}>
+         <TouchableOpacity
+            style={{
+              ...styles.botonGrande,
+              backgroundColor: '#5856D6',
+            }}
+            onPress={() => navigation.navigate('PersonaScreen', {
+              id: 1,
+              nombre: 'Pedro',
+            })}
+          >
+          <Icon
+            name="body-outline"
+            color='white'
+            size={35}
+          />
+        <Text style={styles.botonGrandeTexto}>Pedro</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          ...styles.botonGrande,
+          backgroundColor: '#FF9427',
+        }}
+        onPress={() => navigation.navigate('PersonaScreen', {
+          id: 2,
+          nombre: 'Maria',
+        })}
+      >
+        <Icon
+            name="woman-outline"
+            color='white'
+            size={35}
+        />
+        <Text style={styles.botonGrandeTexto}>Maria</Text>
+      </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Pagina1Screen;
