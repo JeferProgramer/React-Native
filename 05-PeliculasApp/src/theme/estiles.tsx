510a1f41eb0564eebbe7517366fedc8d72a5b6fd
JeferProgramer <jefer.talento.11@gmail.com>
2022-10-26 13:03:15 -0500

    feat add styles in theme

---
+++ 05-PeliculasApp/src/theme/estiles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+export const styles = StyleSheet.create({
+    tituloDetalle:{
+        fontSize: 23,
+        marginTop: 10,
+        fontWeight: 'bold',
+        color:'black',
+    },
+    contenidoDetalle: {
+        fontSize: 18,
+        color: 'black',
+    },
+});
