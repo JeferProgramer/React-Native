0b9968e834c10c6bfbb104578e5a1dab6af6902c
JeferProgramer <jefer.talento.11@gmail.com>
2022-10-27 11:56:30 -0500

    feat add fade screen

---
+++ 05-PeliculasApp/src/screens/FadeScreen.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { View , Animated, Button} from 'react-native';
+import { useFade } from '../hooks/useFade';
+
+export const FadeScreen = () => {
+
+    const {fadeIn,fadeOut,opacity} = useFade();
+
+    return (
+        <View style={{
+            flex: 1,
+            backgroundColor: 'grey',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <Animated.View style={{
+                backgroundColor: '#084F6A',
+                width: 150,
+                height: 150,
+                borderColor: 'white',
+                borderWidth: 10,
+                opacity: opacity,
+            }} />
+            <Button title="Fade In View" onPress={fadeIn} />
+            <Button title="Fade Out View" onPress={fadeOut} />
+        </View>
+    );
+};
