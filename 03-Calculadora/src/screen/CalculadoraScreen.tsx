c842391269612e5612ae94b6bc4b4ff03be1e24b
JeferProgramer <jefer.talento.11@gmail.com>
2022-11-03 11:47:36 -0500

    feat add packege json in app

--- 03-Calculadora/src/screen/CalculadoraScreen.tsx
+++ 03-Calculadora/src/screen/CalculadoraScreen.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, { useEffect } from 'react';
 import {View, Text} from 'react-native';
 import { BotonCalc } from '../components/BotonCalc';
 import { styles } from '../theme/appTheme';
 import { useCalculadora } from '../hooks/useCalculadora';
+import SplashScreen from 'react-native-splash-screen';
 
 
 export const CalculadoraScreen = () => {
@@ -21,6 +22,11 @@ export const CalculadoraScreen = () => {
     calcular,
   } = useCalculadora();
 
+  useEffect(() => {
+     SplashScreen.hide();
+  }, []);
+
+
   return (
     <View style={styles.calculadoraContainer}>
         {
@@ -36,41 +42,41 @@ export const CalculadoraScreen = () => {
 
       {/* Fila Botones*/}
       <View style={styles.fila}>
-        <BotonCalc texto='C' color='#9B9B9B' accion={limpiar}/>
-        <BotonCalc texto='+/-'  color='#9B9B9B' accion={positioNegativo}/>
-        <BotonCalc texto='del'  color='#9B9B9B' accion={btnDelete}/>
-        <BotonCalc texto='/'  color='#FF9427' accion={btnDividir}/>
+        <BotonCalc texto="C" color="#9B9B9B" accion={limpiar}/>
+        <BotonCalc texto="+/-"  color="#9B9B9B" accion={positioNegativo}/>
+        <BotonCalc texto="del"  color="#9B9B9B" accion={btnDelete}/>
+        <BotonCalc texto="/"  color="#FF9427" accion={btnDividir}/>
       </View>
 
       {/* Fila Botones*/}
       <View style={styles.fila}>
-        <BotonCalc texto='7' accion={armarNumero} />
-        <BotonCalc texto='8' accion={armarNumero}/>
-        <BotonCalc texto='9' accion={armarNumero}/>
-        <BotonCalc texto='X'  color='#FF9427' accion={btnMultiplicar}/>
+        <BotonCalc texto="7" accion={armarNumero} />
+        <BotonCalc texto="8" accion={armarNumero}/>
+        <BotonCalc texto="9" accion={armarNumero}/>
+        <BotonCalc texto="X"  color="#FF9427" accion={btnMultiplicar}/>
       </View>
 
        {/* Fila Botones*/}
       <View style={styles.fila}>
-        <BotonCalc texto='4' accion={armarNumero}/>
-        <BotonCalc texto='5' accion={armarNumero}/>
-        <BotonCalc texto='6' accion={armarNumero}/>
-        <BotonCalc texto='-'  color='#FF9427' accion={btnRestar}/>
+        <BotonCalc texto="4" accion={armarNumero}/>
+        <BotonCalc texto="5" accion={armarNumero}/>
+        <BotonCalc texto="6" accion={armarNumero}/>
+        <BotonCalc texto="-"  color="#FF9427" accion={btnRestar}/>
       </View>
 
        {/* Fila Botones*/}
       <View style={styles.fila}>
-        <BotonCalc texto='1' accion={armarNumero} />
-        <BotonCalc texto='2' accion={armarNumero}/>
-        <BotonCalc texto='3' accion={armarNumero}/>
-        <BotonCalc texto='+'  color='#FF9427' accion={btnSumar}/>
+        <BotonCalc texto="1" accion={armarNumero} />
+        <BotonCalc texto="2" accion={armarNumero}/>
+        <BotonCalc texto="3" accion={armarNumero}/>
+        <BotonCalc texto="+"  color="#FF9427" accion={btnSumar}/>
       </View>
 
        {/* Fila Botones*/}
       <View style={styles.fila}>
-        <BotonCalc texto='0' ancho accion={armarNumero}/>
-        <BotonCalc texto='.' accion={armarNumero}/>
-        <BotonCalc texto='='  color='#FF9427' accion={calcular}/>
+        <BotonCalc texto="0" ancho accion={armarNumero}/>
+        <BotonCalc texto="." accion={armarNumero}/>
+        <BotonCalc texto="="  color="#FF9427" accion={calcular}/>
       </View>
 
 
