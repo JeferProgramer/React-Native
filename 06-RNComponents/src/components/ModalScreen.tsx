42fa1372d78439d662e137c7991ced43faf5f2c9
JeferProgramer <jefer.talento.11@gmail.com>
2022-11-04 15:52:53 -0500

    feat add modal screen

---
+++ 06-RNComponents/src/components/ModalScreen.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
+import { Button, Modal, Text, View } from 'react-native';
+import { styles } from '../theme/appTheme';
+import HeaderTitle from './HeaderTitle';
+
+const ModalScreen = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <View style={styles.globalMargin}>
+        <HeaderTitle title="Modal Screen"/>
+
+        <Button
+            title="Abrir modal"
+            onPress={() => setIsVisible(true)}
+        />
+
+        <Modal
+            animationType="slide"
+            visible={isVisible}
+        >
+            <View style={{
+                flex: 1,
+                backgroundColor: 'rgba(0,0,0,0.3)',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                {/* //Contenedor del modal */}
+                <View style={{
+                    width: 200,
+                    height: 200,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    shadowOffset: {
+                        width: 0,
+                        height: 10,
+                    },
+                    shadowOpacity: 0.25,
+                    elevation: 10,
+                    backgroundColor: 'white',
+                    borderRadius: 5,
+                }}>
+                    <Text style={{fontSize: 20, fontWeight: 'bold', color: 'black'}}>Modal</Text>
+                    <Text style={{fontSize: 16, fontWeight: '300', marginBottom: 20, color: 'black'}}>Cuerpo del Modal</Text>
+                    <Button
+                        title="Cerrar"
+                        onPress={() => setIsVisible(false)}
+                    />
+                </View>
+            </View>
+        </Modal>
+    </View>
+  );
+};
+
+export default ModalScreen;
