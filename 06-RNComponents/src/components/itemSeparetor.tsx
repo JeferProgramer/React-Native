088006e48067d095dd1c72922575fb538d33219f
JeferProgramer <jefer.talento.11@gmail.com>
2022-11-04 15:53:22 -0500

    feat add itemseparetor component finish

---
+++ 06-RNComponents/src/components/itemSeparetor.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react-native/no-inline-styles */
+ import React, { useContext } from 'react';
+import { View } from 'react-native';
+import { ThemeContext } from '../context/theme/ThemeContext';
+
+
+export const ItemSeparator = () => {
+
+    const {theme:{divideColor}} = useContext(ThemeContext);
+
+    return (
+      <View style={{
+        borderBottomWidth: 1,
+        opacity: 0.4,
+        marginVertical: 8,
+        borderBottomColor: divideColor,
+      }} />
+    );
+  };
