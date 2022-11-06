ba7ee2527c45acc96df3dcdc3a830abc0b8e381c
JeferProgramer <jefer.talento.11@gmail.com>
2022-11-06 12:19:56 -0500

    feat add alert screen finish

---
+++ 06-RNComponents/src/screens/AlertScreen.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { Alert, Button, View } from 'react-native';
+import HeaderTitle from '../components/HeaderTitle';
+import { styles } from '../theme/appTheme';
+import prompt from 'react-native-prompt-android';
+
+export const AlertScreen = () => {
+  const showAlert = () => {
+    Alert.alert(
+        'Titulo',
+        'Este es el mensaje de la alerta',
+        [
+            {
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+            },
+            {text: 'OK', onPress: () => console.log('OK PRESSED')},
+        ],
+        {
+            cancelable: true,
+            onDismiss: () => console.log('onDismiss'),
+        }
+    );
+  };
+
+  const showPrompt = () => {
+    // Alert.prompt(
+    //     'Estas seguro?',
+    //     'Esta accion no se puede revertir',
+    //     (valor: string) => console.log('info: ', valor),
+    //     'plain-text',
+    //     'Hola Mundo'
+    // );
+    prompt(
+    'Enter password',
+    'Enter your password to claim your $1.5B in lottery winnings',
+    [
+     {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+     {text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password)},
+    ],
+    {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder',
+    }
+    );
+  };
+
+    return (
+    <View style={styles.globalMargin}>
+        <HeaderTitle title="Alerts"/>
+
+        <Button
+            title="Mostrar Alerta"
+            onPress={showAlert}
+        />
+
+        <View style={{height: 10}}/>
+
+         <Button
+            title="Mostrar Prompt"
+            onPress={showPrompt}
+        />
+    </View>
+  );
+};
