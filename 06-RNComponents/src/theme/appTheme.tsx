593d244e6c1c35b6d9119e86e95f158b1fb09ba2
JeferProgramer <jefer.talento.11@gmail.com>
2022-11-06 12:17:07 -0500

    feat finish theme by styles

---
+++ 06-RNComponents/src/theme/appTheme.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    globalMargin:{
+        marginHorizontal: 20,
+    },
+    title : {
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    switchRow:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  switchText: {
+    fontSize: 25,
+  },
+});
