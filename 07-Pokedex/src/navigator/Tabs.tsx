830a87cfa3d5d6343f38ce3bdb834bc00015e736
JeferProgramer <jefer.talento.11@gmail.com>
2022-11-10 21:59:14 -0500

    feat add navigator tabs

---
+++ 07-Pokedex/src/navigator/Tabs.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Navigator } from './Tab1';
+import { Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Tab2Screen } from './Tab2';
+
+const Tab = createBottomTabNavigator();
+
+export const  Tabs = () =>  {
+  return (
+    <Tab.Navigator
+        screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: '#5856D6',
+            tabBarLabelStyle: {
+                marginBottom: (Platform.OS === 'ios') ? 0 : 10,
+            },
+            tabBarStyle: {
+                position: 'absolute',
+                backgroundColor: 'rgba(255,255,255,0.92)',
+                borderWidth: 0,
+                elevation: 0,
+                height: (Platform.OS === 'ios' ? 80 : 60),
+            },
+        }}
+        sceneContainerStyle={{
+            backgroundColor: 'white',
+        }}
+
+    >
+      <Tab.Screen
+        name="Navigator"
+        component={Navigator}
+        options={{
+            tabBarLabel: 'Listado',
+            tabBarIcon: ({color}) => (
+                <Icon
+                    color={color}
+                    size={30}
+                    name="list-outline"
+                />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="SearchScreen"
+        component={Tab2Screen}
+        options={{
+            tabBarLabel: 'Busqueda',
+            tabBarIcon: ({color}) => (
+                <Icon
+                    color={color}
+                    size={30}
+                    name="search-outline"
+                />
+            ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
